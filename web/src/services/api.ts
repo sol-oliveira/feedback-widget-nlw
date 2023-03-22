39d2ff1fec8d68601f02274c809b194e35db405f
Sol Oliveira <sole-oliveira@hotmail.com>
2023-03-21 22:40:47 -0300

    bug: error cors

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+    "Acess-Control-Allow-Origin": "*",
+    Authorization: ``,
+    Accept: "application/json",
+  },
   baseURL: import.meta.env.VITE_API_URL,
 });
